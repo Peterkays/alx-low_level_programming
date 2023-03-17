02c902bcbfe5e65e7d3ba3cb41811daa7a06bcee
Peter <peterkasinamunda@gmail.com>
2023-03-17 01:13:02 -0700

    numbers with puchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: alway returns 0
+ *
+ */
+int main(void)
+{
+	int a;
+
+	for (0; a <= 9 ; a++)
+		putchar('0' + a);
+	putchar('\n');
+	return (0);
+}
