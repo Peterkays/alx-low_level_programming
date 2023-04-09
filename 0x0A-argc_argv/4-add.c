60269656e0000a89f4462ac6ad98c3febc886256
Peter <peterkasinamunda@gmail.com>
2023-04-09 16:15:45 -0700

    addition in argc argv

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,21 +11,21 @@
  */
 int main(int argc, char *argv[])
 {
-    int i, j, sum = 0;
+	int i, j, sum = 0;
 
-    for (i = 1; i < argc; i++)
-    {
-        for (j = 0; argv[i][j]; j++)
-        {
-            if (!isdigit(argv[i][j]))
-            {
-                printf("Error\n");
-                return (1);
-            }
-        }
-        sum += atoi(argv[i]);
-    }
-    printf("%d\n", sum);
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j]; j++)
+	{
+		if (!isdigit(argv[i][j]))
+		{
+		printf("Error\n");
+		return (1);
+		}
+	}
+	sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
 
-    return (0);
+	return (0);
 }
