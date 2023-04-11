78e77f24d40838d5b907d5a4955ddf881405a0c9
Peter <peterkasinamunda@gmail.com>
2023-04-11 06:28:14 -0700

    splits a string into words

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -113,7 +113,7 @@ char **strtow(char *str)
 		if (str[i] != ' ')
 		{
 			pos = _strlen(str, i);
-			tmp[j] = malloc(sizeof(char) * (pos + 1));
+			tmp[j] = malloc(sizeof(char) * (pos + 2));
 			if (tmp[j] == NULL)
 			{
 				for (t = j; t >= 0; t--)
