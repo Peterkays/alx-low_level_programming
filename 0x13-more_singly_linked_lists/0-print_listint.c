9767ee3cc43d060a667970de3ee1875f90a22816
Peter <peterkasimamunda.com>
2023-05-02 01:45:35 -0700

    singly linked according to betty

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,22 +1,19 @@
-#include <stdio.h>
 #include "lists.h"
-
 /**
- * print_listint - Print out the integer contained in a listint_t node
- * @h: Pointer to the start of the list
- *
- * Return: Number of nodes in the list
+ * print_listint - prints integers in list
+ * @h: struct listint_t type
+ * Return: size_t value
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t nodecount;
+	size_t count = 0;
 
-	nodecount = 0;
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
-		nodecount++;
+		count++;
 		h = h->next;
 	}
-	return (nodecount);
-}
+
+	return (count);
+}Z
