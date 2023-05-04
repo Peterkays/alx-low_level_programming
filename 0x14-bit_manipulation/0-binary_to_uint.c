8b33e73b75d436052db5aff2d5ee6fa4c637935a
Peter <peterkasimamunda.com>
2023-05-04 15:09:59 -0700

    bitwise

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,25 +1,28 @@
-#include "main.h"
+#include " ain.h"
 /**
  * binary_to_uint - binary to uint
  * @b: string to convert
  * Return: unsigned int
  *
  */
+
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int x = 0;
+	unsigned int num = 0;
 
 	if (b == NULL)
 		return (0);
+
 	while (*b)
 	{
-		if (*b == '1')
-			x = (x << 1) | 1;
-		else if (*b == '0')
-			x <<= 1;
-		else
+		if (*b != '0' && *b != '1')
 			return (0);
+
+		num <<= 1;
+		if (*b == '1')
+			num += 1;
 		b++;
 	}
-	return (x);
+
+	return (num);
 }
