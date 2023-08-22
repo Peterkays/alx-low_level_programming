74046c48df0db0abb6d98838f3a1eda504bb3b4c
Peter <peterkasinamunda@gmail.com>
2023-08-22 02:24:58 -0700

    fixing access rights

--- 0x18-dynamic_libraries/0-main.c
+++ 0x18-dynamic_libraries/0-main.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 int main(void)
 {
-	
-	printf("At the end of the days, my goal was to be the best hacker");
-return (0);
+
+        printf("At the end of the days, my goal was to be the best hacker");
+	return (0);
 }
